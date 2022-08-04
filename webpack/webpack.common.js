9df3444968350ec4717c220efa02d862533e7f1e
manhnguyen1998 <nguyen.huu.manh@classi.jp>
2022-08-04 22:13:35 +0900

    add icons

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,60 +1,77 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const browser = process.env.BROWSER;
-const BUILD_DIR_NAME = 'dist';
-const SRC_DIR_NAME = 'src';
+const BUILD_DIR_NAME = "dist";
+const SRC_DIR_NAME = "src";
 
 module.exports = {
   entry: {
     popup: path.join(__dirname, `../${SRC_DIR_NAME}/popup.ts`),
-    background: path.join(__dirname, `../${SRC_DIR_NAME}/background/${browser}/background.ts`),
+    background: path.join(
+      __dirname,
+      `../${SRC_DIR_NAME}/background/${browser}/background.ts`
+    ),
   },
   output: {
     path: path.join(__dirname, `../${BUILD_DIR_NAME}`),
-    filename: '[name].js',
+    filename: "[name].js",
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         // required to prevent errors from Svelte on Webpack 5+
         test: /node_modules\/svelte\/.*\.mjs$/,
         resolve: {
-          fullySpecified: false
-        }
-      }
+          fullySpecified: false,
+        },
+      },
     ],
   },
   resolve: {
     alias: {
-      svelte: path.dirname(require.resolve('svelte/package.json'))
+      svelte: path.dirname(require.resolve("svelte/package.json")),
     },
-    extensions: ['.mjs', '.js', '.ts', '.svelte'],
-    mainFields: ['svelte', 'browser', 'module', 'main']
+    extensions: [".mjs", ".js", ".ts", ".svelte"],
+    mainFields: ["svelte", "browser", "module", "main"],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].css'
+      filename: "[name].css",
+      filename: "[name].scss",
     }),
     new CopyPlugin({
       patterns: [
-        { from: './images', to: `../${BUILD_DIR_NAME}/images`, context: 'public' },
-        { from: './bootstrap', to: `../${BUILD_DIR_NAME}/bootstrap`, context: 'public' },
-        { from: './popup.html', to: `../${BUILD_DIR_NAME}/popup.html`, context: 'public' },
-        { from: `${browser}_manifest.json`, to: `../${BUILD_DIR_NAME}/manifest.json`, context: 'public' },
+        {
+          from: "./images",
+          to: `../${BUILD_DIR_NAME}/images`,
+          context: "public",
+        },
+        {
+          from: "./bootstrap",
+          to: `../${BUILD_DIR_NAME}/bootstrap`,
+          context: "public",
+        },
+        {
+          from: "./popup.html",
+          to: `../${BUILD_DIR_NAME}/popup.html`,
+          context: "public",
+        },
+        {
+          from: `${browser}_manifest.json`,
+          to: `../${BUILD_DIR_NAME}/manifest.json`,
+          context: "public",
+        },
       ],
     }),
   ],
